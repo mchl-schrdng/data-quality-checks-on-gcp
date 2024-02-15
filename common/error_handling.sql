69fe9c4845dae206ca86035fdea1bcdc438273af
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2024-02-15 08:49:16 +0100

    Update error_handling.sql

--- common/error_handling.sql
+++ common/error_handling.sql
@@ -1,35 +1,15 @@
-CREATE OR REPLACE PROCEDURE `project_id.dataset.error_logging`(
-    project_name STRING,
-    dataset_name STRING,
-    procedure_name STRING,
+CREATE OR REPLACE PROCEDURE `project_id.dataset.error_handling`(
+    error_project_name STRING,
+    error_dataset_name STRING,
+    error_table_name STRING,
+    error_source STRING,
     error_message STRING
 )
 BEGIN
-    -- Variables to hold the current timestamp.
-    DECLARE current_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP();
-
-    -- Define the name of the error log table.
-    DECLARE error_log_table_name STRING DEFAULT 'error_log';
-
-    -- Attempt to create the error log table if it does not exist.
+    DECLARE full_table_name STRING DEFAULT CONCAT(error_project_name, '.', error_dataset_name, '.', error_table_name);
+    
     EXECUTE IMMEDIATE CONCAT(
-        'CREATE TABLE IF NOT EXISTS `', project_name, '`.`, dataset_name, '`.`', error_log_table_name, '` (',
-        '`logged_at` TIMESTAMP, ',
-        '`procedure_name` STRING, ',
-        '`error_message` STRING',
-        ')'
-    );
-
-    -- Insert the error message into the error log table.
-    EXECUTE IMMEDIATE CONCAT(
-        'INSERT INTO `', project_name, '`.`, dataset_name, '`.`', error_log_table_name, '` (',
-        '`logged_at`, ',
-        '`procedure_name`, ',
-        '`error_message`',
-        ') VALUES (',
-        'TIMESTAMP "', CAST(current_timestamp AS STRING), '", ',
-        '"', procedure_name, '", ',
-        '"', REPLACE(error_message, '"', '\"'), '"',  -- Escape double quotes in the error message.
-        ')'
+        'INSERT INTO `', full_table_name, '` (error_source, error_message, error_timestamp) ',
+        'VALUES("', error_source, '", "', error_message, '", CURRENT_TIMESTAMP())'
     );
 END;
